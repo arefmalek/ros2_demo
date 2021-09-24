414010e4bb6f4a9cd8555a54c28a72ca260a49ff
arefmalek <arefmalek02@gmail.com>
2021-09-24 00:23:55 -0400

    I should be able to graph, matplot errors rn

--- src/py_pubsub_graphing/py_pubsub_graphing/subscriber_member_function.py
+++ src/py_pubsub_graphing/py_pubsub_graphing/subscriber_member_function.py
@@ -23,13 +23,6 @@ import matplotlib.pyplot as plt
 import matplotlib.animation as animate
 
 
-fig = plt.figure()
-ax1 = fig.add_subplot(1,1,1)
-
-times = []
-vals = []
-
-
 class RandomValueSubscriber(Node):
 
     def __init__(self):
@@ -42,38 +35,53 @@ class RandomValueSubscriber(Node):
                 )
         self.subscription  # prevent unused variable warning
         self.i = 0
-        ani = animate.FuncAnimation(fig, self.listener_callback,
-                    interval=1000)
-        plt.show(block=False)
- 
+        # data from talker
+        self.vals = []
+        self.times = []
+
     def listener_callback(self, msg):
         self.get_logger().info('I heard: "%d"' % msg.data)
 
+        self.vals.append(msg.data)
+        self.times.append(self.i)
 
-        vals.append(msg.data)
-        times.append(self.i)
-
-        print(vals, times)
-        if len(vals) > 30:
-            vals.pop(0)
-        if len(times) > 30:
-            times.pop(0)
-
-        ax1.clear()
-        ax1.plot(times, vals)
+        while len(self.vals) > 30:
+            self.vals.pop(0)
+        while len(self.times) > 30:
+            self.times.pop(0)
 
         self.i += 1
+
+
+#graphing plots
+def animate(i, minimal_subscriber):
+    rclpy.spin_once(minimal_subscriber)
+
+
+    ax1.clear()
+    ax1.plot(xs, ys)
 
 def main(args=None):
     rclpy.init(args=args)
 
     minimal_subscriber = RandomValueSubscriber()
 
-    rclpy.spin(minimal_subscriber)
- 
+    fig = plt.figure()
+    ax1 = fig.add_subplot(1,1,1)
+   
+    while 1:
+        rclpy.spin_once(minimal_subscriber)
+
+        print(minimal_subscriber.vals)
+        ax1.clear()
+        ax1.plot(minimal_subscriber.times, minimal_subscriber.vals)
+
+    #rclpy.spin(minimal_subscriber)
+
     # Destroy the node explicitly
     # (optional - otherwise it will be done automatically
     # when the garbage collector destroys the node object)
+    plt.show()
     minimal_subscriber.destroy_node()
     rclpy.shutdown()
 
